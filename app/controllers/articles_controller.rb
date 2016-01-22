f20f747627e9b08b8511266fb125093da93917f3
bear30920 <bear30920@gmail.com>
2016-01-22 17:07:31 +0800

    second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
     def index
         @articles = Article.all
-        #all方法會article models抓出所有article 的文章
+        #all方法會article model抓出所有article 的文章
         #index 字元要是變複數
         if params[:title].present?
+            #當你在搜尋標題,欄位才會起作用
             @articles=@articles.where("title like ?","%#{params[:title]}%")
+            # title like 搜尋tilte 像??
+            #SQL語法加上％只要前後有相似的字會抓出來
         end
         
         if params[:content].present?
@@ -15,19 +18,19 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
-        #宣告變數 從article models去搜尋所對應網頁的參數ID
+        #宣告變數 從article model去搜尋所對應網頁的參數ID
     end
 
     def new
         @article = Article.new
-        #宣告變數為新的article models的實例
+        #宣告變數為新的article model的實例
     end
 
     def create
         @article = Article.new(article_params)
         #宣告article變數
         #article_params接收表單寫的參數
-        #從article models新增出來
+        #從article model新增出來
         if @article.save
             #收到true新的資訊儲存起來
             #save 會回傳 true or false
@@ -41,11 +44,12 @@ class ArticlesController < ApplicationController
 
     def edit
         @article = Article.find(params[:id])
-          #宣告變數 從article models去搜尋所對應網頁的參數ID
+          #宣告變數 從article model去搜尋所對應網頁的參數ID
     end
 
     def update
         @article = Article.find(params[:id])
+        #宣告變數 從article model去搜尋所對應網頁的參數ID
         if @article.update(article_params)
             #使用update方法
             #article_params更新表單寫的參數
@@ -57,6 +61,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article = Article.find(params[:id])
+        #宣告變數 從article model去搜尋所對應網頁的參數ID
         @article.destroy
         #刪除文章
         redirect_to root_path
@@ -65,6 +70,7 @@ class ArticlesController < ApplicationController
 
 
     private
+
     def article_params
         params.require(:article).permit(:title, :content)
         #定義一個方法 接收來自article  只允許存取title,content
